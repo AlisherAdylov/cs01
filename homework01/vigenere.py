5a8b2ead7b3e02944c898514ca3d61c4080f601a
AlisherAdylov <71011295+AlisherAdylov@users.noreply.github.com>
2020-12-16 00:34:26 +0500

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,7 +10,33 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    k = list(keyword)
+    n = len(k)
+    a = list(plaintext)
+    b = len(a)
+    for i in range (0, b):
+        while n < b:
+            k.append(k[i])
+            n += 1 
+            i += 1 
+    for i in range (0, b):
+        if a[i].isalpha() == False:
+            ciphertext += str(a[i])
+            continue
+        if a[i].istitle():
+            if k[i].istitle():
+                shift = int(ord(k[i])) - int(ord("A")) 
+                ciphertext += str(chr((ord(a[i]) + shift - ord("A")) % 26 + ord("A"))) 
+            else:
+                shift = int(ord(k[i])-32) - int(ord("A"))
+                ciphertext += str(chr((ord(a[i]) + shift - ord("A")) % 26 + ord("A"))) 
+        else:
+            if k[i].istitle():
+                shift = int(ord(k[i])+32) - int(ord("a"))
+                ciphertext += str(chr((ord(a[i]) + shift - ord("a")) % 26 + ord("a")))
+            else:
+                shift = int(ord(k[i])) - int(ord("a")) 
+                ciphertext += str(chr((ord(a[i]) + shift - ord("a")) % 26 + ord("a")))
     return ciphertext
 
 
@@ -26,5 +52,31 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    k = list(keyword)
+    n = len(k)
+    a = list(ciphertext)
+    b = len(a)
+    for i in range (0, b):
+        while n < b:
+            k.append(k[i])
+            n += 1 
+            i += 1 
+    for i in range (0, b):
+        if a[i].isalpha() == False:
+            plaintext += str(a[i])
+            continue
+        if a[i].istitle():
+            if k[i].istitle():
+                shift = int(ord(k[i])) - int(ord("A")) 
+                plaintext += str(chr((ord(a[i]) - shift - ord("A")) % 26 + ord("A"))) 
+            else:
+                shift = int(ord(k[i])-32) - int(ord("A"))
+                plaintext += str(chr((ord(a[i]) - shift - ord("A")) % 26 + ord("A"))) 
+        else:
+            if k[i].istitle():
+                shift = int(ord(k[i])+32) - int(ord("a"))
+                plaintext += str(chr((ord(a[i]) - shift - ord("a")) % 26 + ord("a")))
+            else:
+                shift = int(ord(k[i])) - int(ord("a")) 
+                plaintext += str(chr((ord(a[i]) - shift - ord("a")) % 26 + ord("a")))
     return plaintext
