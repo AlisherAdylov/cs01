9ef318a8018a138c83d7595c6485adc07cb78a00
AlisherAdylov <71011295+AlisherAdylov@users.noreply.github.com>
2021-05-30 14:50:58 +0300

    Update scoring.py

--- homework06/scoring.py
+++ homework06/scoring.py
@@ -6,11 +6,11 @@ from naive_bayes.db import News, session
 
 s = session()
 rows = s.query(News).all()
-stop_sign = int(0.7 * len(rows))  # train-to-test ratio is usually 70:30
+stop_sign = int(0.7 * len(rows))
 extracts: tp.List[str] = []
 labels: tp.List[str] = []
 for i in range(len(rows)):
-    row = s.query(News).filter(News.id == (i + 1)).first()  # id is unique and enumerated from id
+    row = s.query(News).filter(News.id == (i + 1)).first()
     extracts.append(row.title)
     labels.append(row.label)
 extracts = [stemmer.clear(x).lower() for x in extracts]
@@ -18,5 +18,5 @@ X_train, X_test = extracts[:stop_sign], extracts[stop_sign:]
 y_train, y_test = labels[:stop_sign], labels[stop_sign:]
 model = bayes.NaiveBayesClassifier(alpha=0.05)
 model.fit(X_train, y_train)
-print("Точность: ", end="")
+print("Точность вычислений: ", end="")
 print(model.score(X_test, y_test))
